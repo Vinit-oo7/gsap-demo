d73c49b9b2f5d4a4630c857126241d077589fc8e
VinitBhatnagar <vinitbhatnagar2024.katargam@gmail.com>
2025-12-09 13:08:26 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useEffect } from "react";
+import gsap from "gsap";
 
-function App() {
+export default function App() {
+  const boxRef = useRef(null);
+
+useEffect(() => {
+let tl = gsap.timeline({repeat: -1, repeatDelay: 1, yoyo: true})
+  tl.from(boxRef.current, { opacity: 0, scale: 0.5, duration: 1 ,borderRadius:100})
+    .to(boxRef.current, { x: 300, duration: 1 ,opacity:1})
+    .to(boxRef.current, { rotation: 360, duration: 1,scale:1,opacity:0,borderRadius:0});
+}, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div
+      ref={boxRef}
+      style={{
+        width: 100,
+        height: 100,
+        borderRadius:0,
+        background: "teal",
+        // margin: 50
+      }}
+    ></div>
   );
 }
 
-export default App;
